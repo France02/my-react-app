f9a319b5b1e5b19e2ca0a17dc4e9d54457151f34
Francesco Golino <francescogolino02@gmail.com>
2025-02-10 19:56:22 +0100

    Aggiunta roba

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React from 'react';
+import logo from ".../icons_assets/logo.svg"
 
-const Header = () =>{
-    return(
-        <header>
-            <h1>Little Lemon</h1>
-        </header>
-    );
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img src={logo} alt="Little Lemon Logo" />
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#menu">Menu</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
