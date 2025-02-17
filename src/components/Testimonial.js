44abee337ec95e9affb18fd0ee616ad23d6eba23
Francesco Golino <francescogolino02@gmail.com>
2025-02-17 13:43:22 +0100

    Aggiunto lo stile

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,23 +1,26 @@
 import React from 'react';
-
+import avatar from '../icons_assets/3daf33e4-d6ea-47d4-aab5-84bdbae898d7.png'
 const Testimonial = () => {
   // Array di testimonianze
   const testimonials = [
     {
       id: 1,
-      avatar: "/assets/testimonial-avatar.jpg",
+      star: "⭐⭐⭐⭐",
+      avatar: avatar,
       text: "Un'esperienza culinaria fantastica! Tornerò sicuramente.",
       author: "Maria Rossi",
     },
     {
       id: 2,
-      avatar: "/assets/testimonial-avatar.jpg",
+      star: "⭐⭐⭐⭐",
+      avatar: avatar,
       text: "Servizio eccellente e cibo delizioso. Consigliatissimo!",
       author: "Luca Bianchi",
     },
     {
       id: 3,
-      avatar: "/assets/testimonial-avatar.jpg",
+      star: "⭐⭐⭐⭐",
+      avatar: avatar,
       text: "Un ambiente accogliente e un menu vario. Perfetto!",
       author: "Anna Verdi",
     },
@@ -25,13 +28,14 @@ const Testimonial = () => {
 
   return (
     <section className="testimonial">
-      <h2>Cosa dicono i nostri clienti</h2>
+      <h2>Our customers love us!</h2>
       <div className="testimonial-grid container">
         {testimonials.map((item) => (
-          <div key={item.id} className="testimonial-card">
+          <div key={item.id} className="testimonial-card">           
             <img src={item.avatar} alt={`Avatar di ${item.author}`} />
             <p>{item.text}</p>
-            <p>- {item.author}</p>
+            <p>- {item.author}</p> 
+            <p>{item.star}</p>
           </div>
         ))}
       </div>
