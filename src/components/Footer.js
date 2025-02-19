818a026fa0e0945e252c43cb36555185f7ceafa9
Francesco Golino <francescogolino02@gmail.com>
2025-02-19 15:01:31 +0100

    Aggiunto la funzione Routes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,40 @@
 import React from 'react';
-import logo from '../icons_assets/Logo.svg'
+import { Link } from 'react-router-dom';
+import logo from '../icons_assets/Logo.svg';
+
 const Footer = () => {
   return (
     <footer className="footer">
-    <div className="footer-section">
-      <h3>Little Lemon</h3>
-      <img src={logo} alt="Little Lemon Logo" className="footer-logo"/>
-    </div>
-    <div className="footer-section">
-      <h4>Doormat Navigation</h4>
-      <ul className="footer-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Order Online</a></li>
-        <li><a href="#">Reservations</a></li>
-        <li><a href="#">Login</a></li>
-      </ul>
-    </div>
-    <div className="footer-section">
-      <h4>Contacts</h4>
-      <p>123 Main Street, Chicago</p>
-      <p>+1 (123) 456-7890</p>
-      <p>info@littlelemon.com</p>
-    </div>
-    <div className="footer-section">
-      <h4>Social Media Links</h4>
-      <ul className="footer-social">
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">Instagram</a></li>
-        <li><a href="#">Twitter</a></li>
-      </ul>
-    </div>
-  </footer>
+      <div className="footer-section">
+        <h3>Little Lemon</h3>
+        <img src={logo} alt="Little Lemon Logo" className="footer-logo" />
+      </div>
+      <div className="footer-section">
+        <h4>Doormat Navigation</h4>
+        <ul className="footer-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/menu">Menu</Link></li>
+          <li><Link to="/order-online">Order Online</Link></li>
+          <li><Link to="/reservations">Reservations</Link></li>
+          <li><Link to="/login">Login</Link></li>
+        </ul>
+      </div>
+      <div className="footer-section">
+        <h4>Contacts</h4>
+        <p>123 Main Street, Chicago</p>
+        <p>+1 (123) 456-7890</p>
+        <p>info@littlelemon.com</p>
+      </div>
+      <div className="footer-section">
+        <h4>Social Media Links</h4>
+        <ul className="footer-social">
+          <li><Link to="#">Facebook</Link></li>
+          <li><Link to="#">Instagram</Link></li>
+          <li><Link to="#">Twitter</Link></li>
+        </ul>
+      </div>
+    </footer>
   );
 };
 
