594d0d5c03c58b9d7a400504c0416d0d78373d48
Francesco Golino <francescogolino02@gmail.com>
2025-03-28 13:49:12 +0100

    Aggiunto lazy

--- src/components/Highlights.js
+++ src/components/Highlights.js
@@ -12,19 +12,19 @@ const Highlights = () => {
       </div>
       <div className="highlight-items">
         <div className="highlight-item">
-          <img src={salad} alt="Dish 1" />
+          <img src={salad} alt="Dish 1" loading='lazy' />
           <h4>Greek salad</h4>
           <p>$12.99</p>
           <p>The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons. </p>
         </div>
         <div className="highlight-item">
-          <img src={bruschetta} alt="Dish 1" />
+          <img src={bruschetta} alt="Dish 1" loading='lazy'/>
           <h4>Bruchetta</h4>
           <p>$5.99</p>
           <p>Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. </p>
         </div>
         <div className="highlight-item">
-          <img src={dessert} alt="Dish 1" />
+          <img src={dessert} alt="Dish 1" loading='lazy' />
           <h4>Lemon Dessert</h4>
           <p>$5.00</p>
           <p>This comes straight from grandma's recipe book, every last ingredient has been sourced and is as authentic as can be imagined. </p>
